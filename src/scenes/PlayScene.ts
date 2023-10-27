bb5a4180fdc8815bb10659b94cde134f0490dae1
Marco Colia <marco.colia@fiftydeg.com>
2023-10-27 18:27:24 +0200

    clean code into different functions

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -25,57 +25,12 @@ class PlayScene extends GameScene {
     create() {
         this.createEnvironment()
         this.createPlayer()
+        this.createObstacles()
+        this.createGameOverContainer()
 
-        this.obstacles = this.physics.add.group()
-
-        this.gameOverText =  this.add.image(0,0, 'game-over')
-        this.restartText =  this.add.image(0,80, 'restart')
-
-        this.gameOverContainer = this.add
-            .container(this.gameWidth / 2,(this.gameHeight / 2) - 50)
-            .add([this.gameOverText, this.restartText])
-            .setAlpha(0)
-
-        this.startTrigger = this.physics.add
-            .sprite(0, 10, null)
-            .setAlpha(0)
-            .setOrigin(0, 1)
-
-        this.physics.add.collider(this.obstacles, this.player, () => {
-            this.isGameRunning = false
-            this.physics.pause()
-            this.player.die()
-            this.gameOverContainer.setAlpha(1)
-
-            this.spawnTime = 0
-            this.gameSpeed = 2
-        })
-
-        this.physics.add.overlap(this.startTrigger, this.player, () => {
-            if (this.startTrigger.y === 10) {
-                this.startTrigger.body.reset(0, this.gameHeight)
-                return;
-            }
-
-            this.startTrigger.body.reset(9999, 9999)
-
-            const groundRollOutEvent = this.time.addEvent({
-                delay: 1000 / 60,
-                loop: true,
-                callback: () => {
-                    this.player.playRunAnimation()
-                    this.player.setVelocityX(80)
-                    this.ground.width += (17 * 2)
-
-                    if (this.ground.width >= this.gameWidth) {
-                        groundRollOutEvent.remove()
-                        this.ground.width = this.gameWidth
-                        this.player.setVelocityX(0)
-                        this.isGameRunning = true
-                    }
-                }
-            })
-        })
+        this.handleGameStart()
+        this.handleObstacleCollision()
+        this.handleGameRestart()
     }
     // delta is time from the last frame, time is the entire time
     update(time: number, delta: number) {
@@ -110,6 +65,22 @@ class PlayScene extends GameScene {
             .setOrigin(0, 1)
     }
 
+    createObstacles() {
+        this.obstacles = this.physics.add.group()
+    }
+
+    createGameOverContainer() {
+        this.gameOverText =  this.add.image(0,0, 'game-over')
+        this.restartText =  this.add
+            .image(0,80, 'restart')
+            .setInteractive()
+
+        this.gameOverContainer = this.add
+            .container(this.gameWidth / 2,(this.gameHeight / 2) - 50)
+            .add([this.gameOverText, this.restartText])
+            .setAlpha(0)
+    }
+
     spawnObstacles() {
         const cactuses = Phaser.Math.Between(1, PRELOAD_CONFIG.cactusCount)
         const distance = Phaser.Math.Between(600,900)
@@ -118,6 +89,57 @@ class PlayScene extends GameScene {
             .create(distance, this.gameHeight, `cactus-${cactuses}`)
             .setImmovable()
             .setOrigin(0,1)
+    }
+
+    handleGameStart() {
+        this.startTrigger = this.physics.add
+            .sprite(0, 10, null)
+            .setAlpha(0)
+            .setOrigin(0, 1)
+
+        this.physics.add.overlap(this.startTrigger, this.player, () => {
+            if (this.startTrigger.y === 10) {
+                this.startTrigger.body.reset(0, this.gameHeight)
+                return;
+            }
+
+            this.startTrigger.body.reset(9999, 9999)
+
+            const groundRollOutEvent = this.time.addEvent({
+                delay: 1000 / 60,
+                loop: true,
+                callback: () => {
+                    this.player.playRunAnimation()
+                    this.player.setVelocityX(80)
+                    this.ground.width += (17 * 2)
+
+                    if (this.ground.width >= this.gameWidth) {
+                        groundRollOutEvent.remove()
+                        this.ground.width = this.gameWidth
+                        this.player.setVelocityX(0)
+                        this.isGameRunning = true
+                    }
+                }
+            })
+        })
+    }
+
+    handleGameRestart() {
+        this.restartText.on('pointerdown', () => {
+
+        })
+    }
+
+    handleObstacleCollision() {
+        this.physics.add.collider(this.obstacles, this.player, () => {
+            this.isGameRunning = false
+            this.physics.pause()
+            this.player.die()
+            this.gameOverContainer.setAlpha(1)
+
+            this.spawnTime = 0
+            this.gameSpeed = 2
+        })
     }
 }
 
